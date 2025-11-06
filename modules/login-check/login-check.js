54f64418bac4f9eafde139727d6c41f8a9d9b398
jnswkz <23520408@gm.uit.edu.vn>
2025-11-06 12:07:30 +0700

    fix: ensure alert and redirect only occur on dashboard access when not logged in

--- modules/login-check/login-check.js
+++ modules/login-check/login-check.js
@@ -1,6 +1,8 @@
 if (localStorage.getItem('isLoggedIn') !== 'true') {
-    alert('You must be logged in to access this page.');
-    window.location.href = '/pages/login.html';
+    if (window.location.pathname.endsWith('dashboard.html')) {
+        alert('You must be logged in to access this page.');
+        window.location.href = '/pages/login.html';
+    }
 }
 else {
     if (window.location.pathname.endsWith('index.html')){
